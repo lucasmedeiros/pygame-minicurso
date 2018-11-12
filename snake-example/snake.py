3d934f9abf362582a64d2bdeaf36aa867ae7d0bc
LukeHxH <lucasmed812@gmail.com>
2018-11-12 18:55:30 -0300

    finaliza implementação do jogo de snake

--- snake-example/snake.py
+++ snake-example/snake.py
@@ -7,6 +7,10 @@ pygame.init()
 WIDTH = 800
 HEIGHT = 600
 
+UPPER_BOUND_X = 790
+UPPER_BOUND_Y = 590
+LOWER_BOUND = 0
+
 COLOR_BLACK = (0, 0, 0)
 COLOR_RED = (255, 0, 0)
 COLOR_WHITE = (255, 255, 255)
@@ -16,23 +20,32 @@ LEFT = 1
 DOWN = 2
 RIGHT = 3
 
+font = pygame.font.SysFont('comicsansms', 25)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Minicurso Pygame - Snake")
+
+# creates a unique model for text display
 def text_objects(text, font, color):
 	text_surface = font.render(text, True, color)
 	return text_surface, text_surface.get_rect()
 
 # generates random positions for apples
 def on_grid_rand_position():
-    x = random.randint(0, 790)
-    y = random.randint(0, 590)
+    x = random.randint(LOWER_BOUND, UPPER_BOUND_X)
+    y = random.randint(LOWER_BOUND, UPPER_BOUND_Y)
+
+    # print (x)
+    # print (y)
+
+    # print (x//10 * 10)
+    # print (y//10 * 10)
 
     return (x//10 * 10, y//10 * 10)
 
 # checks collision between two cells
 def collision(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Minicurso Pygame - Snake")
 
 snake = [(200, 200), (210, 200), (220, 200)]
 
@@ -50,13 +63,14 @@ close = False
 
 clock = pygame.time.Clock()
 
-font = pygame.font.SysFont('comicsansms', 25)
-
 surface_gameover_text, rect_gameover_text = text_objects("GAME OVER", font, COLOR_WHITE)
 rect_gameover_text.center = ((WIDTH / 2), (HEIGHT / 15))
 
 surface_pause_text, rect_pause_text = text_objects("PAUSE", font, COLOR_WHITE)
 rect_pause_text.center = ((WIDTH / 2), (HEIGHT / 15))
+
+surface_tryagain_text, rect_tryagain_text = text_objects("[ENTER] PARA TENTAR NOVAMENTE", font, COLOR_WHITE)
+rect_tryagain_text.center = ((WIDTH / 2), (HEIGHT / 2))
 
 # game main loop
 while not close:
@@ -75,8 +89,10 @@ while not close:
                 curr_direction = LEFT
             if (event.key == pygame.K_RIGHT) and (not curr_direction == LEFT):
                 curr_direction = RIGHT
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and not game_over:
                 pause = (not pause)
+            if event.key == pygame.K_RETURN:
+                game_over = False
     
     if not game_over:
         if not pause:
@@ -106,12 +122,18 @@ while not close:
             apple_pos = on_grid_rand_position()
             snake.append((0, 0))
         
-        screen.blit(apple, apple_pos)
+        if snake[0][0] < LOWER_BOUND or snake[0][0] > UPPER_BOUND_X or snake[0][1] < LOWER_BOUND or snake[0][1] > UPPER_BOUND_Y:
+            game_over = True
 
         for pos in snake:
             screen.blit(snake_skin, pos)
+
+        screen.blit(apple, apple_pos)
     else:
         screen.blit(surface_gameover_text, rect_gameover_text)
+        screen.blit(surface_tryagain_text, rect_tryagain_text)
+        snake = [(200, 200), (210, 200), (220, 200)]
+        curr_direction = RIGHT
 
     pygame.display.update()
     clock.tick(30)
