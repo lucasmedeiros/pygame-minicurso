fc4434d86c1e656a23228988d05deed6917fb1c6
LukeHxH <lucasmed812@gmail.com>
2018-11-28 14:28:34 -0300

    documentação do template principal

--- implementations/template.py
+++ implementations/template.py
@@ -12,7 +12,7 @@ GAME_NAME = "Template do Projeto - Minicurso de Pygame"
 WIDTH = 480
 HEIGHT = 600
 
-# taxa de FPS em que o programa será atulizado
+# definição taxa de FPS em que o programa será atulizado
 FPS = 30
 
 # definição de cores
@@ -23,48 +23,78 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 class Game:
-    # TODO iniciar o jogo
+    """
+    classe usada para representar o jogo e todas as suas funcionalidades
+
+    Attributes
+    ----------
+    screen : pygame.Surface
+        uma superfície para a tela principal do jogo
+    running : bool
+        valor que representa se o loop está rodando ou não
+    all_sprites : pygame.sprite.Group
+        grupo de sprites utilizados no jogo.
+    """
 
     def __init__(self):
         pygame.init() # inicia pygame
-        pygame.mixer.init() # inicia mixer (sons do pygame)
+        pygame.mixer.init()
 
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption(GAME_NAME)
 
         self.running = True
-        self.clock = pygame.time.Clock()
         self.all_sprites = pygame.sprite.Group()
     
-    # manipula os eventos
     def handle_events(self):
+        """
+            handle_events() -> None gerencia os eventos do jogo
+        """
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
 
-    # atualizações do jogo
     def update(self):
+        """
+            update() -> None atualiza todas as informações do jogo
+        """
         self.all_sprites.update()
+        pygame.display.update()
 
-    # desenhar / renderizar no jogo
     def draw(self):
+        """
+            draw() -> None desenha todos os sprites na tela
+        """
         self.all_sprites.draw(self.screen)
         pygame.display.flip()
 
-    # loop principal do jogo
     def loop(self):
+        """
+            loop() -> None roda o loop principal do jogo
+        """
+
+        clock = pygame.time.Clock()
+
         while self.running:
-            self.clock.tick(FPS)
+            clock.tick(FPS)
 
             self.handle_events()
             self.update()
             self.draw()
-        
+    
+    def run(self):
+        # TODO coisas para fazer antes do loop iniciar
+
+        self.loop()
+
+        # TODO coisas para fazer depois que o loop acabar
+
         pygame.quit()
 
 def main():
     game = Game()
-    game.loop()
+    game.run()
 
 if __name__ == "__main__":
     main()
